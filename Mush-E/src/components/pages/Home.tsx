b63da0780d84d24d468d9f4282a507d32bb5f709
shaburu <xshaburu@gmail.com>
2023-09-13 00:14:05 +0530

    Last Commit of the day

--- Mush-E/src/components/pages/Home.tsx
+++ Mush-E/src/components/pages/Home.tsx
@@ -1,5 +1,5 @@
 import viteLogo from '/mascaria.png'
-
+import green from '/bokchoy.png'
 import "./Home.css";
 
 import { Link } from "react-router-dom";
@@ -11,48 +11,84 @@ export const Home = () => {
       <div className='hero'>
           <div className='content-left'>
             <h1>
-              Computer Vision 4 Mushrooms
+             Mushrooms Bot 
             </h1>
             <p>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere eligendi error eos
             laudantium necessitatibus illo! Eius laudantium doloremque animi, 
             velit incidunt ullam nulla voluptates alias labore earum dolorum corporis corrupti?
             </p>
-            <button>
+            <div className='TryCode'>            
+              <Link to="/mushroom">
+              <div>
               Try Now
-            </button>
-            <button>
-              View Code
-            </button>        
+              </div>
+              </Link>
+              <a href='#'>
+              <div>
+              View Code           
+              </div> 
+              </a>
+            </div>      
           </div>
           
-          <div className='content-right'>
-          <Link to="/green">
+          <div className='content-right logo'>
+          <Link to="/mushroom">
           <img src={viteLogo} className="logo react" alt="React logo" />
           </Link>
+          </div> 
+        </div>
+
+        <div className='hero'>
+          <div className='content-right2 logo'>
+          <Link to="/mushroom">
+          <img src={green} className="logo vite" alt="React logo" width='100%' />
+          </Link>
           </div>
-          
-          
+          <div className='content-left'>
+            <h1>
+              Leafy Greens Bot
+            </h1>
+            <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere eligendi error eos
+            laudantium necessitatibus illo! Eius laudantium doloremque animi, 
+            velit incidunt ullam nulla voluptates alias labore earum dolorum corporis corrupti?
+            </p>
+            <div className='TryCode'>            
+              <Link to="/green">
+              <div>
+              Try Now
+              </div>
+              </Link>
+              <a href='#'>
+              <div>
+              View Code           
+              </div> 
+              </a>
+            </div>      
+          </div> 
         </div>
         
         
         
-        <div className="flex">
+        
+        {/* <div className="flex">
           <div className="flex-child">
           <Link to="/green">
-            <img src={viteLogo} className="logo react" alt="React logo" />
+            <img src={green} className="logo vite" alt="React logo" />
             </Link>
           </div>
           <div className="flex-child flex flex--vertical">
             <div className="flex-child">
-              <h1>right top</h1>
+              <h1>Computer Vision 4 Leafy Greens</h1>
             </div>
             <div className="flex-child">
-              <h1>right bottom</h1>
+              <h1></h1>
               <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium autem esse iste voluptate eum ex mollitia temporibus unde eveniet omnis, vel, corrupti sed nobis consequatur quaerat ad sequi aliquid nostrum?</p>
             </div>
           </div>
-        </div>
+        </div> */}
+
 </div>
 
   
